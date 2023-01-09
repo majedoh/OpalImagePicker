87719bd13b4acf045b5b05ef4b6fe3b10516a62d
Majed Hariri <majedoh@gmail.com>
2023-01-09 13:13:29 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "OpalImagePicker",
-        dependencies : []
+    products: [.library(name: "OpalImagePicker", targets: ["OpalImagePicker"])],
+    dependencies : []
 )
